627a7550ea3b2eb5b7077a4a98688f6d22629f9f
Lukas Winkler <l.winkler23@mailbox.org>
2018-03-10 21:02:40 +0100

    optional skipping by score

--- parsexml.py
+++ parsexml.py
@@ -10,12 +10,16 @@ from bs4 import BeautifulSoup
 BASEDIR = utils.get_settings(1)
 
 i = 0
+skipped = 0
 iterator = ElementTree.iterparse(BASEDIR + "/Posts.xml")
 with jsonlines.open(BASEDIR + '/Questions.jsonl', mode="w") as questions, \
         jsonlines.open(BASEDIR + '/Answers.jsonl', mode="w") as answers, \
         jsonlines.open(BASEDIR + "/Titles.jsonl", "w") as titles:
     for event, element in iterator:
         title = element.get('Title')
+        # if element.get('Score') and int(element.get('Score')) > 2:
+        #     skipped += 1
+        #     continue
         if title:
             titles.write(title)
         body = element.get('Body')
@@ -31,4 +35,5 @@ with jsonlines.open(BASEDIR + '/Questions.jsonl', mode="w") as questions, \
             print(i)
         i += 1
 print("{number} total entries".format(number=i))
+print("{number} skipped".format(number=skipped))
 print("used {mb}MB".format(mb=resource.getrusage(resource.RUSAGE_SELF).ru_maxrss // 1024))
