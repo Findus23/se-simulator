253df7927b3568601bd48fbbc6587542d36cf336
Lukas Winkler <l.winkler23@mailbox.org>
2018-04-29 09:31:19 +0200

    add sample config

---
+++ config.sample.py
@@ -0,0 +1,11 @@
+# database must be "se-simulator"
+db = {
+    "host": "localhost",
+    "user": "username",
+    "passwd": "password",
+}
+
+# check http://flask.pocoo.org/docs/0.12/quickstart/#sessions on how to create
+secret_key = 
+
+production = False
