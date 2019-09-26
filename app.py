5b60afbb5e06d34c3de138c1148ecae569373139
Lukas Winkler <git@lw1.at>
2019-09-26 10:25:37 +0200

    use new sentry SDK

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 # Blog configuration values.
-
+import sentry_sdk
 from flask import Flask
 from playhouse.flask_utils import FlaskDB
 from playhouse.pool import PooledMySQLDatabase
+from sentry_sdk.integrations.flask import FlaskIntegration
 
 import config
 
 DATABASE = PooledMySQLDatabase("se-simulator", **config.db)
 
+if config.sentryDSN:
+    sentry_sdk.init(
+        dsn=config.sentryDSN,
+        integrations=[FlaskIntegration()]
+    )
 
 # Create a Flask WSGI app and configure it using values from the module.
 app = Flask(__name__)
